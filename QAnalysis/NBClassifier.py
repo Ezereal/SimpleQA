ade5c5699bf6decfdcb016e82ce57cf26b63fab6
Ezereal <793532302@qq.com>
2017-11-13 19:41:32 +0800

    finish NBClassifier.py

--- QAnalysis/NBClassifier.py
+++ QAnalysis/NBClassifier.py
@@ -32,9 +32,75 @@ class NBClassifier(object):
         dictFeaturesBase = {}
 
         for subTuple in trainSet:
-            for key,value in subTuple[0].items():
-                if key not in dictFeaturesBase.keys():
-                    dictFeaturesBase[key] = {value:1}
+            for feature in subTuple[0]:
+                if feature not in dictFeaturesBase.keys():
+                    dictFeaturesBase[feature] = 1
                 else:
-                    if value not in dictFeaturesBase[key].keys():
-                        
+                    dictFeaturesBase[feature] += 1
+
+        dictFeatures = {}.fromkeys([key for key in dictTag])
+
+        for key in dictFeatures.keys():
+            dictFeatures[key] = {}.fromkeys([key for key in dictFeaturesBase])
+
+        for subTuple in trainSet:
+            for feature in subTuple[0]:
+                dictFeatures[subTuple[1]][feature] = 1 if dictFeatures[subTuple[1]][feature] == None else dictFeatures[subTuple[1]][feature] + 1
+
+        # 将训练样本中没有的项目，由None改为一个非常小的数值，表示其概率极小而并非是零
+        for tag,feature in dictFeatures.items():
+            for key,value in feature.items():
+                if value == None:
+                    feature[key] = 0
+
+        # 由特征频率计算特征的条件概率P(feature|tag)
+        for tag,fearure in dictFeatures.items():
+            totalValue = sum([x for x in feature.values() if x != None])
+            for key,value in feature.items():
+                feature[key] = value / totalValue if value != None else None
+
+        self.featuresProbablity = dictFeatures
+
+
+    def classify(self,features):
+        resultDict = {}
+
+        # 计算每个tag的条件概率
+        for key,value in self.tagProbablity.items():
+
+            iNumList = []
+            for feature in features:
+                if self.featuresProbablity[key][feature]:
+                    iNumList.append(self.featuresProbablity[key][feature])
+
+            conditionPr = 1
+            for iNum in iNumList:
+                conditionPr *= iNum
+            resultDict[key] = value * conditionPr
+
+        resultList = sorted(resultDict.items(),key = lambda x:x[1],reverse=True)
+        return resultList[0][0]
+
+if __name__ == '__main__':
+
+    trainSet = [
+        ({"是","谁"},"人物"),
+        ({"在","哪"},"地点"),
+        ({"哪里"}, "地点"),
+        ({"什么","地方"}, "地点"),
+        ({"多少"}, "数字"),
+        ({"多长"}, "数字"),
+        ({"多","宽"}, "数字"),
+        ({"什么","时候"}, "时间"),
+        ({"何时"}, "时间"),
+        ({"什么","东西"}, "实体"),
+        ({"是","什么"}, "实体"),
+        ({"因为","什么"}, "描述"),
+        ({"为什么"}, "描述"),
+        ({"原因","是"}, "描述")
+    ]
+
+    classifier = NBClassifier()
+    classifier.train(trainSet)
+    result = classifier.classify({"多少"})
+    print(result)
